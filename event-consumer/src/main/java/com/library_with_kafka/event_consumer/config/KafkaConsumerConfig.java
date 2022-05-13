da853622d2d14a7b65b4c9cf324f42b0356e32d6
ArturSydor <sydor.artur@ukr.net>
2022-05-13 18:53:11 +0300

    Setup kafka consumer

---
+++ event-consumer/src/main/java/com/library_with_kafka/event_consumer/config/KafkaConsumerConfig.java
@@ -0,0 +1,9 @@
+package com.library_with_kafka.event_consumer.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+
+@Configuration
+@EnableKafka
+public class KafkaConsumerConfig {
+}
